4e344dac009f38bf5d946b2ed79d2ec0b9b892aa
K C <aries0420@gmail.com>
2013-07-28 14:39:03 -0700

    Generated the bugtracker app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bugtracker::Application.config.secret_token = '0c34465c3080f05a5eb6647c7e7dd5285804266256980927b269a59206d52421874433a8d0a50cca2ff1ef2ab51968e2058a3afd58968055a073fdb29af45b42'
