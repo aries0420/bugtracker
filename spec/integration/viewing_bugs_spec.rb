f8669284ae526ab0ecfc636d23fcf50830481890
K C <aries0420@gmail.com>
2013-07-28 22:08:30 -0700

    Adding functionality to display a list of bugs relevant to the specific game products

---
+++ spec/integration/viewing_bugs_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature "Viewing bugs" do
+  before do
+    candycrush = Factory(:product, :title => "Candy Crush")
+    Factory(:bug,
+            :product => candycrush,
+            :title => "Fix the bug!",
+            :description => "This is one of the coolest games ever!",
+            :priority => "High",
+            :status => "Open",
+            :reporter => "Anonymous")
+
+    angrybird = Factory(:product, :title => "Angry Bird")
+    Factory(:bug,
+            :product => angrybird,
+            :title => "Animation freezes the screen",
+            :description => "Isn't a joke.",
+            :priority => "Medium",
+            :status => "Open",
+            :reporter => "Fan")
+
+    visit '/'
+  end
+
+  scenario "Viewing bugs for a given product" do
+    click_link "Candy Crush"
+    page.should have_content("Fix the bug!")
+    page.should_not have_content("Animation freezes the screen")
+
+    click_link "Fix the bug!"
+    within("#bug h2") do
+      page.should have_content("Fix the bug!")
+    end
+    page.should have_content("This is one of the coolest games ever!")
+  end
+end
---
+++ spec/support/factories/bug_factory.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :bug do
+    title "Bug example"
+    description "An example bug, nothing more"
+    priority "test"
+    status "test"
+    reporter "test"
+  end
+end
